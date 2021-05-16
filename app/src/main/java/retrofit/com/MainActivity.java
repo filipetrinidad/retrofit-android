91ef1897104310fda6255d1410fb6c05ee45b5d8
filipetrinidad <fipe5766@gmail.com>
2021-05-15 23:38:17 -0300

    post-put-delete

--- app/src/main/java/retrofit/com/MainActivity.java
+++ app/src/main/java/retrofit/com/MainActivity.java
@@ -1,8 +1,6 @@
 package retrofit.com;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,16 +18,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botao;
+    private Button botao1, botao2;
     private TextView texto;
     private Retrofit retrofit;
+    private ServicePost service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botao = findViewById(R.id.botao1);
+        botao1 = findViewById(R.id.botao1);
         texto = findViewById(R.id.text);
 
         retrofit = new Retrofit.Builder()
@@ -37,12 +36,51 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        service = retrofit.create(ServicePost.class);
 
-        botao.setOnClickListener(new View.OnClickListener() {
+        botao1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //recuperaCEP();
-                postar();
+                excluir();
+            }
+        });
+
+    }
+
+    private void excluir(){
+
+        Call<Void> call = service.excluirDelete(2);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()){
+                    texto.setText("Status: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void atualizar(){
+
+        Call<JsonPlace> call = service.atualizarPut(2);
+        call.enqueue(new Callback<JsonPlace>() {
+            @Override
+            public void onResponse(Call<JsonPlace> call, Response<JsonPlace> response) {
+                if(response.isSuccessful()){
+                    JsonPlace jsonPlace2 = response.body();
+                    texto.setText(response.code() + " " + jsonPlace2.getId());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonPlace> call, Throwable t) {
+
             }
         });
     }
@@ -51,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
         JsonPlace jsonPlace = new JsonPlace("123", "aasasa", "asasasas");
 
-        ServicePost service = retrofit.create(ServicePost.class);
         Call<JsonPlace> call = service.realizarPost( jsonPlace );
 
         call.enqueue(new Callback<JsonPlace>() {
@@ -70,7 +107,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private  void recuperaCEP(){
+   /* private  void recuperaCEP(){
+
         ServiceCEP service = retrofit.create(ServiceCEP.class);
         Call<CEP> call = service.obtemCEP();
 
@@ -88,5 +126,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
+    }*/
 }
--- app/src/main/java/retrofit/com/api/ServicePost.java
+++ app/src/main/java/retrofit/com/api/ServicePost.java
@@ -3,10 +3,19 @@ package retrofit.com.api;
 import retrofit.com.model.JsonPlace;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ServicePost {
 
      @POST("/posts")
     Call<JsonPlace> realizarPost(@Body JsonPlace jsonPlace);
+
+     @PUT("/posts/{id}")
+    Call<JsonPlace> atualizarPut(@Path("id") int id);
+
+     @DELETE("/posts/{id}")
+    Call<Void> excluirDelete(@Path("Id") int id );
 }
